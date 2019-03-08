645602a383f90bbc843c1460e32779326b62ac61
Szambelan <p.szambelan1@gmail.com>
2019-03-08 10:33:05 +0100

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        System.out.println("mesage");
+
         String fileName = "ai-lab1-ttp_data/student/trivial_0.ttp";
         ArrayList<City> listOfCities = new ArrayList<>();
         ArrayList<Item> listOfItems = new ArrayList<>();
